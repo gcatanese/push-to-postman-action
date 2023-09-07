2c4a8b7f87648de24e9975e6fd893bae8b98bed7
Beppe Catanese <gcatanese@yahoo.com>
2023-09-07 15:31:02 +0200

    Add source

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import core from '@actions/core';
+
+import {Postman} from './postman.js'
+
+init()
+
+async function init () {
+
+    try {
+
+        const postmanApiKey = core.getInput('postman-key');
+        if(!postmanApiKey) {
+            throw new Error("Missing input postman-key");
+        }
+
+        const postmanFile = core.getInput('postman-file');
+        if(!postmanFile) {
+            throw new Error("Missing input postman-file");
+        }
+
+        const workspaceId = core.getInput('workspace-id');
+        const collectionId = core.getInput('collection-id');
+        if(!workspaceId && !collectionId) {
+            throw new Error("Provide either collection-id or workspace-id");
+        }
+
+        if(collectionId) {
+            updateCollection(postmanApiKey, postmanFile, collectionId);
+        } else if (workspaceId) {
+            createCollection(postmanApiKey, postmanFile, workspaceId);
+        }
+
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+async function updateCollection(postmanApiKey, postmanFile, collectionId) {
+    new Postman(postmanApiKey).updateCollection(postmanFile, collectionId);
+}
+
+async function createCollection(postmanApiKey, postmanFile, collectionId) {
+    new Postman(postmanApiKey).createCollection(postmanFile, workspaceId);
+}
+
---
+++ src/postman.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import fs from "fs";
+
+const URL = "https://api.getpostman.com/collections";
+
+export class Postman {
+
+    constructor(postmanApiKey) {
+        this.postmanApiKey = postmanApiKey;
+    }
+
+    createCollection(postmanFile, workspaceId) {
+
+        const createCollectionUrl = URL + "?workspace=" + workspaceId;
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-API-Key': this.postmanApiKey,
+            },
+        };
+
+        console.log("Push (post) to Postman");
+
+        axios
+            .post(createCollectionUrl, this.getFileAsJson(postmanFile), config)
+            .then(res => {
+                console.log(`statusCode: ${res.statusCode}`)
+                console.log(res)
+            })
+            .catch(error => {
+                throw error;
+            })
+
+    }
+
+    updateCollection(postmanFile, collectionId) {
+
+        const updateCollectionUrl = URL + "/" + collectionId;
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-API-Key': this.postmanApiKey,
+            },
+        };
+
+        console.log("Push (put) to Postman");
+
+        axios
+            .put(updateCollectionUrl, this.getFileAsJson(postmanFile), config)
+            .then(res => {
+                console.log(`statusCode: ${res.statusCode}`)
+                console.log(res)
+            })
+            .catch(error => {
+                throw error;
+            })
+
+    }
+
+    getFileAsJson(postmanFile) {
+        const collection = JSON.parse(fs.readFileSync(postmanFile, 'utf8'));
+        return json = JSON.stringify({
+            "collection": collection,
+        });
+
+    }
+}
+
